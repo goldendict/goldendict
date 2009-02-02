9f41c3304ac1630607b89130267f3cc9147b773f
Konstantin Isakov <ikm@users.berlios.de>
2009-02-02 01:04:39 +0000

    Adopt to a different prototype of the iconv() function under Windows.

--- src/iconv.cc
+++ src/iconv.cc
@@ -37,7 +37,12 @@ Iconv::Result Iconv::convert( void const * & inBuf, size_t  & inBytesLeft,
   throw( exIncorrectSeq, exOther )
 {
   size_t result = iconv( state,
-                         (char **)&inBuf, &inBytesLeft,
+                         #ifdef __WIN32
+                         (char const **)&inBuf,
+                         #else
+                         (char **)&inBuf,
+                         #endif
+                                           &inBytesLeft,
                          (char **)&outBuf, &outBytesLeft );
 
   if ( result == (size_t) -1 )
