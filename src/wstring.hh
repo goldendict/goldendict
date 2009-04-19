99a9431295e1aa5339105252e6b81e9f63c348b9
Konstantin Isakov <ikm@users.berlios.de>
2009-04-19 23:34:49 +0000

    * Comments edited a bit.

--- src/wstring.hh
+++ src/wstring.hh
@@ -9,7 +9,7 @@
 /// While most systems feature a 4-byte wchar_t and an UCS-4 Unicode
 /// characters representation for it, Windows uses 2-byte wchar_t and an UTF-16
 /// encoding. The use of UTF-16 on Windows is most probably a homeage to an
-/// ancient history dating back to when there was nothing by a BLP, and
+/// ancient history dating back to when there was nothing but a BMP, and
 /// all Unicode chars were 2 bytes long. After the Unicode got expanded past
 /// two-byte representation, the guys at Microsoft had probably decided that
 /// the least painful way to go is to just switch to UTF-16. Or so's the theory.
@@ -25,18 +25,21 @@
 /// and uses UTF-16 instead. Neither solution allows for direct processing of
 /// the symbols in those strings without decoding them first. And the 16-bit
 /// solution is even more ugly than the 8-bit one, because it doesn't have a
-/// benefit of ISO-8859-1 compatibility, having a much more useless BLP
+/// benefit of ASCII compatibility, having a much more useless UCS-2
 /// compatibility instead. It's stuck in the middle of nowhere, really.
 /// 
 /// The question is, what are we going to do with all this? When we do Unicode
 /// processing in GoldenDict, we want to use real Unicode charaters, not some
 /// UTF-16 encoded ones. To that end, we have two options under Windows: first,
-/// use QString, and second, use basic_string< uint32_t >.
+/// use QString, and second, use basic_string< unsigned int >.
 /// While we use QStrings for the GUI and other non-critical code, there is a
 /// serious doubt on the efficiency of QStrings for bulk text processing. And
 /// since a lot of code uses wstring already, it would be much easier to convert
-/// it to use basic_string< uint32_t > instead, since it shares the same
-/// template, and therefore the interface too, with wstring.
+/// it to use basic_string< unsigned int > instead, since it shares the same
+/// template, and therefore the interface too, with wstring. That's why we
+/// introduce our own gd::wstring and gd::wchar types here. On all systems but
+/// Windows, they are equivalent to std::wstring and wchar_t. On Windows, they
+/// are basic_string< unsigned int > and unsigned int.
 
 namespace gd
 {
