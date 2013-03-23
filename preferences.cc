d1787dd98b91b4056af96d87cfa88d7213895709
Tvangeste <i.4m.l33t@yandex.ru>
2013-03-23 17:48:31 +0100

    Added a new built-in style 'Modern' (Prefs->Interface->Display Style->Modern)

--- preferences.cc
+++ preferences.cc
@@ -78,6 +78,7 @@ Preferences::Preferences( QWidget * parent, Config::Preferences const & p ):
     }
 
   ui.displayStyle->addItem( QIcon( ":/icons/programicon.png" ), tr( "Default" ), QString() );
+  ui.displayStyle->addItem( QIcon( ":/icons/programicon.png" ), tr( "Modern" ), QString( "modern" ) );
   ui.displayStyle->addItem( QIcon( ":/icons/icon32_dsl.png" ), tr( "Lingvo" ), QString( "lingvo" ) );
   ui.displayStyle->addItem( QIcon( ":/icons/icon32_bgl.png" ), tr( "Babylon" ), QString( "babylon" ) );
 
