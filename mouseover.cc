8a20ec70da88f4b30cbe63d9eed7dca5654526c7
Abs62 <ottomann@yandex.ru>
2013-09-03 17:59:19 +0400

    Fix compilation with MinGW 4.8

--- mouseover.cc
+++ mouseover.cc
@@ -27,7 +27,7 @@ const UINT WM_MY_SHOW_TRANSLATION = WM_USER + 301;
 static wchar_t className[] = L"GoldenDictMouseover";
 typedef BOOL ( WINAPI  *ChangeWindowMessageFilterFunc )( UINT, DWORD );
 
-#ifndef WINAPI_FAMILY
+#ifndef _MSC_VER
 typedef struct tagCHANGEFILTERSTRUCT {
   DWORD cbSize;
   DWORD ExtStatus;
