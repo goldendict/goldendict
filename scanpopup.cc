goldendict/goldendict

commit 92e8c85eeca078a65c6fd4409b1ebd2832fbb55f

Fix behavior while words list font resizing (issue #1109)

scanpopup.cc
@@ -115,6 +115,7 @@ ScanPopup::ScanPopup( QWidget * parent,
 
   wordListDefaultFont = ui.translateBox->wordList()->font();
   translateLineDefaultFont = ui.translateBox->font();
+  groupListDefaultFont = ui.groupList->font();
 
   applyZoomFactor();
   applyWordsZoomLevel();
@@ -406,9 +407,31 @@ void ScanPopup::applyWordsZoomLevel()
   if ( ui.translateBox->translateLine()->font().pointSize() != ps )
     ui.translateBox->translateLine()->setFont( font );
 
-  ui.groupList->setFont(font);
+  font = groupListDefaultFont;
+  ps = font.pointSize();
 
-  ui.groupList->parentWidget()->layout()->activate();
+  if ( cfg.preferences.wordsZoomLevel != 0 )
+  {
+    ps += cfg.preferences.wordsZoomLevel;
+    if ( ps < 1 )
+      ps = 1;
+    font.setPointSize( ps );
+  }
+
+  if ( ui.groupList->font().pointSize() != ps )
+  {
+    disconnect( ui.groupList, SIGNAL( currentIndexChanged( QString const & ) ),
+                this, SLOT( currentGroupChanged( QString const & ) ) );
+    int n = ui.groupList->currentIndex();
+    ui.groupList->clear();
+    ui.groupList->setFont( font );
+    ui.groupList->fill( groups );
+    ui.groupList->setCurrentIndex( n );
+    connect( ui.groupList, SIGNAL( currentIndexChanged( QString const & ) ),
+             this, SLOT( currentGroupChanged( QString const & ) ) );
+  }
+
+  ui.outerFrame->layout()->activate();
 }
 
 Qt::WindowFlags ScanPopup::unpinnedWindowFlags() const

mainwindow.cc
@@ -316,6 +316,7 @@ MainWindow::MainWindow( Config::Class & cfg_ ):
 
   wordListDefaultFont = wordList->font();
   translateLineDefaultFont = translateLine->font();
+  groupListDefaultFont = groupList->font();
 
   // Make the dictionaries pane's titlebar
   foundInDictsLabel.setText( tr( "Found in Dictionaries:" ) );
@@ -3651,7 +3652,32 @@ void MainWindow::applyWordsZoomLevel()
   if ( translateLine->font().pointSize() != ps )
     translateLine->setFont( font );
 
-  groupList->setFont(font);
+  font = groupListDefaultFont;
+
+  ps = font.pointSize();
+
+  if ( cfg.preferences.wordsZoomLevel != 0 )
+  {
+    ps += cfg.preferences.wordsZoomLevel;
+
+    if ( ps < 1 )
+      ps = 1;
+
+    font.setPointSize( ps );
+  }
+
+  if ( groupList->font().pointSize() != ps )
+  {
+    disconnect( groupList, SIGNAL( currentIndexChanged( QString const & ) ),
+                this, SLOT( currentGroupChanged( QString const & ) ) );
+    int n = groupList->currentIndex();
+    groupList->clear();
+    groupList->setFont( font );
+    groupList->fill( groupInstances );
+    groupList->setCurrentIndex( n );
+    connect( groupList, SIGNAL( currentIndexChanged( QString const & ) ),
+             this, SLOT( currentGroupChanged( QString const & ) ) );
+  }
 
   wordsZoomBase->setEnabled( cfg.preferences.wordsZoomLevel != 0 );
   // groupList->setFixedHeight(translateLine->height());

mainwindow.hh
@@ -116,7 +116,7 @@ private:
   TranslateBox * translateBox;
 
   /// Fonts saved before words zooming is in effect, so it could be reset back.
-  QFont wordListDefaultFont, translateLineDefaultFont;
+  QFont wordListDefaultFont, translateLineDefaultFont, groupListDefaultFont;
 
   QAction escAction, focusTranslateLineAction, addTabAction, closeCurrentTabAction,
           closeAllTabAction, closeRestTabAction,

scanpopup.hh
@@ -139,7 +139,7 @@ private:
   DictionaryBar dictionaryBar;
   MainStatusBar * mainStatusBar;
   /// Fonts saved before words zooming is in effect, so it could be reset back.
-  QFont wordListDefaultFont, translateLineDefaultFont;
+  QFont wordListDefaultFont, translateLineDefaultFont, groupListDefaultFont;
 
 #ifdef HAVE_X11
   ScanFlag * scanFlag;

commit a9fc823c46beb7f364236773a9a9bbbe3c431e41

Avoid duplicate currentGroupChanged() connection
Before this change ScanPopup::applyWordsZoomLevel() was called before
the primary connection to ScanPopup::currentGroupChanged() in
ScanPopup's constructor. This meant that disconnect() had no effect and
connect() established the first connection during the first call to
ScanPopup::applyWordsZoomLevel(). This caused 2 issues:
  1. Since the connection happened before the filling of ui.groupList
in ScanPopup's constructor, the current group in scan popup was always
"All" on Goldendict start.
  2. Since the connection was not unique, the slot was connected twice
to the same signal, and it was actually called twice.

The bug was introduced in 92e8c85eeca078a65c6fd4409b1ebd2832fbb55f.

scanpopup.cc
@@ -117,9 +117,6 @@ ScanPopup::ScanPopup( QWidget * parent,
   translateLineDefaultFont = ui.translateBox->font();
   groupListDefaultFont = ui.groupList->font();
 
-  applyZoomFactor();
-  applyWordsZoomLevel();
-
   ui.mainLayout->addWidget( definition );
 
   ui.translateBox->wordList()->attachFinder( &wordFinder );
@@ -331,6 +328,9 @@ ScanPopup::ScanPopup( QWidget * parent,
   connect( &delayTimer, SIGNAL( timeout() ),
     this, SLOT( delayShow() ) );
 #endif
+
+  applyZoomFactor();
+  applyWordsZoomLevel();
 }
 
 ScanPopup::~ScanPopup()