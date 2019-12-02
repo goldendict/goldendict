48bd4723ea4a472744a36a9c0b8843ca3a8384f1
nonwill <nonwill@hotmail.com>
2019-12-03 00:36:16 +0800

    Friendly notice for FMOD Ex. FMOD_System_Close is not needed when FMOD_System_Release is called.

--- fmodex_player.cc
+++ fmodex_player.cc
@@ -34,7 +34,7 @@ typedef FMOD_RESULT (*FMOD_SYSTEM_CREATE)(FMOD_SYSTEM **system);
 typedef FMOD_RESULT (*FMOD_SYSTEM_RELEASE)(FMOD_SYSTEM *system);
 typedef FMOD_RESULT (*FMOD_SYSTEM_GETVERSION)(FMOD_SYSTEM *system, unsigned int *version);
 typedef FMOD_RESULT (*FMOD_SYSTEM_INIT)(FMOD_SYSTEM *system, int maxchannels, FMOD_INITFLAGS flags, void *extradriverdata);
-typedef FMOD_RESULT (*FMOD_SYSTEM_CLOSE)(FMOD_SYSTEM *system);
+//typedef FMOD_RESULT (*FMOD_SYSTEM_CLOSE)(FMOD_SYSTEM *system);
 typedef FMOD_RESULT (*FMOD_SYSTEM_CREATESOUND)(FMOD_SYSTEM *system, const char *name_or_data, FMOD_MODE mode, FMOD_CREATESOUNDEXINFO *exinfo, FMOD_SOUND **sound);
 typedef FMOD_RESULT (*FMOD_SOUND_SETMODE)(FMOD_SOUND *sound, FMOD_MODE mode);
 typedef FMOD_RESULT (*FMOD_SYSTEM_PLAYSOUND)(FMOD_SYSTEM *system, FMOD_CHANNELINDEX channelid, FMOD_SOUND *sound, FMOD_BOOL paused, FMOD_CHANNEL **channel);
@@ -51,7 +51,7 @@ struct FmodexAudioPlayer::FMOD_EX_API {
     FMOD_SYSTEM_RELEASE FMOD_System_Release;
     FMOD_SYSTEM_GETVERSION FMOD_System_GetVersion;
     FMOD_SYSTEM_INIT FMOD_System_Init;
-    FMOD_SYSTEM_CLOSE FMOD_System_Close;
+    //FMOD_SYSTEM_CLOSE FMOD_System_Close;
     FMOD_SYSTEM_CREATESOUND FMOD_System_CreateSound;
     FMOD_SOUND_SETMODE FMOD_Sound_SetMode;
     FMOD_SYSTEM_PLAYSOUND FMOD_System_PlaySound;
@@ -64,7 +64,7 @@ bool FmodexAudioPlayer::ERRCHECK(int result)
 {
     if (result != FMOD_OK)
     {
-        emit error( tr("FMOD error! (%1) %2").arg(result).arg(FMOD_ErrorString(FMOD_RESULT(result))) );
+        emit error( tr("FMOD ERR: (%1) - %2").arg(result).arg(FMOD_ErrorString(FMOD_RESULT(result))) );
         return true;
     }
     return false;
@@ -84,7 +84,7 @@ FmodexAudioPlayer::FmodexAudioPlayer()
     fmodex->FMOD_System_Release = (FMOD_SYSTEM_RELEASE)fmodex_dl.resolve("FMOD_System_Release");
     fmodex->FMOD_System_GetVersion = (FMOD_SYSTEM_GETVERSION)fmodex_dl.resolve("FMOD_System_GetVersion");
     fmodex->FMOD_System_Init = (FMOD_SYSTEM_INIT)fmodex_dl.resolve("FMOD_System_Init");
-    fmodex->FMOD_System_Close = (FMOD_SYSTEM_CLOSE)fmodex_dl.resolve("FMOD_System_Close");
+    //fmodex->FMOD_System_Close = (FMOD_SYSTEM_CLOSE)fmodex_dl.resolve("FMOD_System_Close");
     fmodex->FMOD_System_CreateSound = (FMOD_SYSTEM_CREATESOUND)fmodex_dl.resolve("FMOD_System_CreateSound");
     fmodex->FMOD_Sound_SetMode = (FMOD_SOUND_SETMODE)fmodex_dl.resolve("FMOD_Sound_SetMode");
     fmodex->FMOD_System_PlaySound = (FMOD_SYSTEM_PLAYSOUND)fmodex_dl.resolve("FMOD_System_PlaySound");
@@ -92,28 +92,27 @@ FmodexAudioPlayer::FmodexAudioPlayer()
     fmodex->FMOD_Channel_Stop = (FMOD_CHANNEL_STOP)fmodex_dl.resolve("FMOD_Channel_Stop");
     fmodex->FMOD_Sound_Release = (FMOD_SOUND_RELEASE)fmodex_dl.resolve("FMOD_Sound_Release");
 
-    FMOD_RESULT result = fmodex->FMOD_System_Create(&fmodex->system);
-    if(ERRCHECK(result))
+    if(ERRCHECK(fmodex->FMOD_System_Create(&fmodex->system)))
         clean();
     else
     {
         unsigned int version = 0;
-        result = fmodex->FMOD_System_GetVersion(fmodex->system, &version);
-        if(ERRCHECK(result))
+        if(ERRCHECK(fmodex->FMOD_System_GetVersion(fmodex->system, &version)))
         {
             clean();
         }
-        if (version < FMOD_VERSION)
+        else if (version < FMOD_VERSION)
         {
             clean();
-            emit error( tr("Error! You are using an old version of FMOD %08x. This program requires %08x").
+            emit error( tr("FMOD ERR: You are using an old version of FMOD %08x. This program requires %08x.").
                         arg(version).arg(FMOD_VERSION) );
         }
-        else
+        else if(ERRCHECK(fmodex->FMOD_System_Init(fmodex->system, 1,
+                                                  FMOD_INIT_STREAM_FROM_UPDATE |
+                                                  FMOD_INIT_SYNCMIXERWITHUPDATE,
+                                                  0)))
         {
-            result = fmodex->FMOD_System_Init(fmodex->system, 1, FMOD_INIT_NORMAL, 0);
-            if(ERRCHECK(result))
-                clean();
+            clean();
         }
     }
 }
@@ -131,10 +130,7 @@ void FmodexAudioPlayer::clean()
     {
         if(fmodex->system)
         {
-            FMOD_RESULT result = fmodex->FMOD_System_Close(fmodex->system);
-            ERRCHECK(result);
-            result = fmodex->FMOD_System_Release(fmodex->system);
-            ERRCHECK(result);
+            ERRCHECK(fmodex->FMOD_System_Release(fmodex->system));
         }
         delete fmodex;
         fmodex = 0;
@@ -144,7 +140,7 @@ void FmodexAudioPlayer::clean()
 QString FmodexAudioPlayer::play( const char * data, int size )
 {
   if(fmodex==nullptr)
-      return tr( "FMOD Ex Library (%1) is not available!" ).arg(fmodex_dyl_name);
+      return tr( "FMOD Ex Library (%1) is invalid or not available!" ).arg(fmodex_dyl_name);
   stop();
   FMOD_CREATESOUNDEXINFO exinfo;
   memset(&exinfo, 0, sizeof(FMOD_CREATESOUNDEXINFO));
@@ -156,7 +152,7 @@ QString FmodexAudioPlayer::play( const char * data, int size )
                                                        &exinfo, &fmodex->sound);
   if(ERRCHECK(result))
   {
-      return tr( "Couldn't create sound for playing." );
+      return tr( "Failed to create sound. The sound type may not be supported by FMOD Ex." );
   }
 
   result = fmodex->FMOD_Sound_SetMode(fmodex->sound, FMOD_LOOP_OFF);
