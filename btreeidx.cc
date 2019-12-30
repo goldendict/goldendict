goldendict/goldendict

commit eec50df270c0b66c9fd2b58c3bfad6535bf77884

Make ensureInitDone easy.

btreeidx.cc
@@ -63,12 +63,10 @@ BtreeDictionary::BtreeDictionary( string const & id,
 {
 }
 
-string const & BtreeDictionary::ensureInitDone()
-{
-  static const string empty;
-
-  return empty;
-}
+//bool BtreeDictionary::ensureInitDone(std::string *err)
+//{
+//  return true;
+//}
 
 void BtreeIndex::openIndex( IndexInfo const & indexInfo,
                             File::Class & file, Mutex & mutex )
@@ -178,9 +176,10 @@ void BtreeWordSearchRequest::findMatches()
     return;
   }
 
-  if ( dict.ensureInitDone().size() )
+  string err;
+  if ( !dict.ensureInitDone(&err) )
   {
-    setErrorString( QString::fromUtf8( dict.ensureInitDone().c_str() ) );
+    setErrorString( QString::fromUtf8( err.c_str() ) );
     finish();
     return;
   }
@@ -462,9 +461,10 @@ void BtreeWordSearchRequest::run()
     return;
   }
 
-  if ( dict.ensureInitDone().size() )
+  string err;
+  if ( dict.ensureInitDone(&err) )
   {
-    setErrorString( QString::fromUtf8( dict.ensureInitDone().c_str() ) );
+    setErrorString( QString::fromUtf8( err.c_str() ) );
     finish();
     return;
   }

aard.cc
@@ -603,7 +603,7 @@ void AardDictionary::makeFTSIndex( QAtomicInt & isCancelled, bool firstIteration
   if( haveFTSIndex() )
     return;
 
-  if( ensureInitDone().size() )
+  if( !ensureInitDone() )
     return;
 
   if( firstIteration && getArticleCount() > FTS::MaxDictionarySizeForFastSearch )

btreeidx.hh
@@ -204,7 +204,7 @@ public:
   /// does nothing.
   /// The function returns an empty string if the initialization is or was
   /// successful, or a human-readable error string otherwise.
-  virtual string const & ensureInitDone();
+  virtual bool ensureInitDone(string *err = 0) { return true; }
 
 protected:
   Mutex ftsIdxMutex;

dictdfiles.cc
@@ -481,7 +481,7 @@ void DictdDictionary::makeFTSIndex( QAtomicInt & isCancelled, bool firstIteratio
   if( haveFTSIndex() )
     return;
 
-  if( ensureInitDone().size() )
+  if( !ensureInitDone() )
     return;
 
   if( firstIteration && getArticleCount() > FTS::MaxDictionarySizeForFastSearch )

dsl.cc
@@ -229,7 +229,7 @@ public:
 
   virtual void setFTSParameters( Config::FullTextSearch const & fts )
   {
-    if( ensureInitDone().size() )
+    if( !ensureInitDone() )
       return;
 
     can_FTS = fts.enabled
@@ -246,7 +246,7 @@ protected:
 
 private:
 
-  virtual string const & ensureInitDone();
+  virtual bool ensureInitDone(string *err = 0);
   void doDeferredInit();
 
   /// Loads the article. Does not process the DSL language.
@@ -372,12 +372,15 @@ void DslDictionary::deferredInit()
 }
 
 
-string const & DslDictionary::ensureInitDone()
+bool DslDictionary::ensureInitDone(string *err)
 {
   // Simple, really.
   doDeferredInit();
-
-  return initError;
+  if(initError.empty())
+    return true;
+  if(err)
+      *err = initError;
+  return false;
 }
 
 void DslDictionary::doDeferredInit()
@@ -1260,7 +1263,7 @@ void DslDictionary::makeFTSIndex( QAtomicInt & isCancelled, bool firstIteration 
   if( haveFTSIndex() )
     return;
 
-  if( ensureInitDone().size() )
+  if( !ensureInitDone() )
     return;
 
   if( firstIteration && getArticleCount() > FTS::MaxDictionarySizeForFastSearch )
@@ -1647,9 +1650,10 @@ void DslArticleRequest::run()
     return;
   }
 
-  if ( dict.ensureInitDone().size() )
+  string err;
+  if ( !dict.ensureInitDone(&err) )
   {
-    setErrorString( QString::fromUtf8( dict.ensureInitDone().c_str() ) );
+    setErrorString( QString::fromUtf8( err.c_str() ) );
     finish();
     return;
   }
@@ -1850,9 +1854,10 @@ void DslResourceRequest::run()
     return;
   }
 
-  if ( dict.ensureInitDone().size() )
+  string err;
+  if ( !dict.ensureInitDone(&err) )
   {
-    setErrorString( QString::fromUtf8( dict.ensureInitDone().c_str() ) );
+    setErrorString( QString::fromUtf8( err.c_str() ) );
     finish();
     return;
   }

epwing.cc
@@ -130,7 +130,7 @@ public:
 
   virtual void setFTSParameters( Config::FullTextSearch const & fts )
   {
-    if( ensureInitDone().size() )
+    if( !ensureInitDone() )
       return;
 
     can_FTS = fts.enabled

ftshelpers.cc
@@ -1147,9 +1147,10 @@ void FTSResultsRequest::fullSearch( QStringList & searchWords, QRegExp & regexp 
 
 void FTSResultsRequest::run()
 {
-  if ( dict.ensureInitDone().size() )
+  string err;
+  if ( !dict.ensureInitDone(&err) )
   {
-    setErrorString( QString::fromUtf8( dict.ensureInitDone().c_str() ) );
+    setErrorString( QString::fromUtf8( err.c_str() ) );
     finish();
     return;
   }

gls.cc
@@ -656,7 +656,7 @@ void GlsDictionary::makeFTSIndex( QAtomicInt & isCancelled, bool firstIteration 
   if( haveFTSIndex() )
     return;
 
-  if( ensureInitDone().size() )
+  if( !ensureInitDone() )
     return;
 
   if( firstIteration && getArticleCount() > FTS::MaxDictionarySizeForFastSearch )

commit 51d3d0609b20b98af4399e50ff627b18f35d35ce

Fix failed to handle the result of WordSearchRequest
This issue was introduced by eec50df270c0b66c9fd2b58c3bfad6535bf77884

btreeidx.cc
@@ -462,7 +462,7 @@ void BtreeWordSearchRequest::run()
   }
 
   string err;
-  if ( dict.ensureInitDone(&err) )
+  if ( !dict.ensureInitDone(&err) )
   {
     setErrorString( QString::fromUtf8( err.c_str() ) );
     finish();