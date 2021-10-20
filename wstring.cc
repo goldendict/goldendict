9857ed0559eeff7e186815b85606837fc00ddbf0
xiaoyifang <yifang.xiao@outlook.com>
2021-10-20 23:02:37 +0800

    use char32_t as the inner string unit.

--- wstring.cc
+++ wstring.cc
@@ -3,12 +3,15 @@
 #include "wstring.hh"
 #include "iconv.hh"
 #include <wchar.h>
+#include <QString>
 
 namespace gd
 {
   wstring __nativeToWs( wchar_t const * str )
   {
-    return Iconv::toWstring( "WCHAR_T", str, wcslen( str ) * sizeof( wchar_t ) );
+      QString qStr=QString::fromWCharArray(str);
+      return qStr.toStdU32String();
+    //return Iconv::toWstring( "WCHAR_T", str, wcslen( str ) * sizeof( wchar_t ) );
   }
 }
 
