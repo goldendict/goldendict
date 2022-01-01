47a8e624a37a13daf5020092d9b430d269d83a9b
yifang <yifang.xiao@outlook.com>
2022-01-01 21:09:55 +0800

    fix:relative url redirect

--- resources/gd_custom.js
+++ resources/gd_custom.js
@@ -2,10 +2,16 @@
 $(function() {
         $("a").click(function(event) {
             var link = $(this).attr("href");
-            if (link.indexOf("://") < 0) {
-                var newLink = window.location.href + "/" + link;
-                $(this).attr("href", newLink);
+            var newLink;
+            if (link.startsWith("#")) {
+                newLink = window.location.href + link;
+            } else {
+                var href = window.location.href;
+                var index = href.indexOf("?");
+                newLink = href.substring(0, index) + "?word=" + link;
             }
+            $(this).attr("href", newLink);
+
         });
 
     }
