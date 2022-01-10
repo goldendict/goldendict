9dc39f77486fe7a2d8ff23a52cea00d64eb0d86b
xiaoyifang <yifang.xiao@outlook.com>
2022-01-10 20:17:22 +0800

    refactor code,use default parameter

--- resourceschemehandler.cpp
+++ resourceschemehandler.cpp
@@ -9,7 +9,7 @@ void ResourceSchemeHandler::requestStarted(QWebEngineUrlRequestJob *requestJob)
 
     QNetworkRequest request;
     request.setUrl(url);
-    QNetworkReply *reply = this->mManager.createRequest(QNetworkAccessManager::GetOperation, request, NULL);
+    QNetworkReply *reply = this->mManager.createRequest(QNetworkAccessManager::GetOperation, request);
     connect(reply, &QNetworkReply::finished, requestJob, [=]() {
       if (reply->error() == QNetworkReply::ContentNotFoundError) {
         requestJob->fail(QWebEngineUrlRequestJob::UrlNotFound);
