850c7d66e87058cdfd4e51b8acf5d44c78a10211
yifang <yifang.xiao@outlook.com>
2022-01-14 08:19:49 +0800

    fix:single click on link,the emulated db click will still work on new page

--- resources/gd_custom.js
+++ resources/gd_custom.js
@@ -3,6 +3,7 @@
 $(function() {
         $("a").click(function(event) {
             var link = $(this).attr("href");
+            emitClickedEvent(link);
             if(link.indexOf(":")>=0){
                 return;
             }
@@ -40,4 +41,14 @@ $(function() {
 function playSound(sound) {
     var a = new Audio(sound);
     a.play();
+}
+
+function emitClickedEvent(link){
+    try{
+        articleview.linkClickedInHtml(link);
+    }catch(error)
+    {
+        console.log(error);
+    }
+
 }
