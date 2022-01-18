65f2125885002936ee201d0b5287eec29f08ebd1
xiaoyifang <yifang.xiao@outlook.com>
2022-01-18 20:47:45 +0800

    clean code:change file encoding from utf-8 bom to utf-8

--- greektranslit.cc
+++ greektranslit.cc
@@ -1,4 +1,4 @@
-﻿/* This file is (c) 2010 Jennie Petoumenou <epetoumenou@gmail.com>
+/* This file is (c) 2010 Jennie Petoumenou <epetoumenou@gmail.com>
  * Part of GoldenDict. Licensed under GPLv3 or later, see the LICENSE file */
 
 #include "greektranslit.hh"
--- romaji.cc
+++ romaji.cc
@@ -1,4 +1,4 @@
-﻿#include "romaji.hh"
+#include "romaji.hh"
 #include <QCoreApplication>
 
 namespace Romaji {
