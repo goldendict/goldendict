b71925787c915ba56a91638464ec60cb4ab9ac7e
xiaoyifang <yifang.xiao@outlook.com>
2022-01-29 14:04:23 +0800

    revert mouse32 changes

--- mouseover_win32/HookImportFunction.h
+++
@@ -1,8 +0,0 @@
-#ifndef _HookImportFunction_H_
-#define _HookImportFunction_H_
-
-#include <windows.h>
-
-BOOL HookAPI(LPCSTR szImportModule, LPCSTR szFunc, PROC paHookFuncs, PROC* paOrigFuncs);
-
-#endif
