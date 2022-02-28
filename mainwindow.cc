c9d053d227cdc6e5dea15da3892789948d5df01f
yifang <yifang.xiao@outlook.com>
2022-02-28 21:21:34 +0800

    fix:edit dictionaries dialog ,memory consume too much

--- mainwindow.cc
+++ mainwindow.cc
@@ -2050,8 +2050,10 @@ void MainWindow::editDictionaries( unsigned editDictionaryGroup )
   closeHeadwordsDialog();
   closeFullTextSearchDialog();
 
-  ftsIndexing.stopIndexing();
-  ftsIndexing.clearDictionaries();
+  // the full index does not have to wait until this function invoked.
+
+//  ftsIndexing.stopIndexing();
+//  ftsIndexing.clearDictionaries();
 
   wordFinder.clear();
   dictionariesUnmuted.clear();
@@ -2108,14 +2110,14 @@ void MainWindow::editDictionaries( unsigned editDictionaryGroup )
   makeScanPopup();
   installHotKeys();
 
-  for( unsigned x = 0; x < dictionaries.size(); x++ )
-  {
-    dictionaries[ x ]->setFTSParameters( cfg.preferences.fts );
-    dictionaries[ x ]->setSynonymSearchEnabled( cfg.preferences.synonymSearchEnabled );
-  }
+//  for( unsigned x = 0; x < dictionaries.size(); x++ )
+//  {
+//    dictionaries[ x ]->setFTSParameters( cfg.preferences.fts );
+//    dictionaries[ x ]->setSynonymSearchEnabled( cfg.preferences.synonymSearchEnabled );
+//  }
 
-  ftsIndexing.setDictionaries( dictionaries );
-  ftsIndexing.doIndexing();
+//  ftsIndexing.setDictionaries( dictionaries );
+//  ftsIndexing.doIndexing();
 }
 
 void MainWindow::editCurrentGroup()
