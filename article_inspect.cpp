65c78be24748958c7c89b918188db7fda7ea32de
xiaoyifang <yifang.xiao@gmail.com>
2022-03-26 21:31:04 +0800

    qt6.2 qt5.15 compatibility

--- article_inspect.cpp
+++ article_inspect.cpp
@@ -1,6 +1,8 @@
 #include "article_inspect.h"
 #include <QCloseEvent>
+#if (QT_VERSION > QT_VERSION_CHECK(6,0,0))
 #include <QWebEngineContextMenuRequest>
+#endif
 ArticleInspector::ArticleInspector( QWidget * parent ) : QDialog( parent, Qt::WindowType::Window )
 {
   setAttribute( Qt::WidgetAttribute::WA_DeleteOnClose, false );
@@ -16,11 +18,15 @@ void ArticleInspector::setInspectPage( QWebEngineView * view )
   auto page=view->page();
   this->inspectedPage = page;
   page->setDevToolsPage( inspectView->page() );
+#if( QT_VERSION > QT_VERSION_CHECK( 6, 0, 0 ) )
   // without this line, application will crash on qt6.2 ,see https://bugreports.qt.io/browse/QTBUG-101724
   if( view->lastContextMenuRequest() )
   {
     page->triggerAction( QWebEnginePage::InspectElement );
   }
+#else
+  page->triggerAction( QWebEnginePage::InspectElement );
+#endif
   raise();
   show();
 }
