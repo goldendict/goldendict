bbc664bc6ce4e348b813e9f4840cfdb2393bfb67
ngn999 <ngn998@gmail.com>
2022-04-15 00:31:37 +0800

    libao: static link libmacosx.so plugin, fix #46
    libao.dylib is universal library now

---
+++ maclibs/include/ao/os_types.h
@@ -0,0 +1,39 @@
+/*
+ *
+ *  os_types.h
+ *
+ *      Original Copyright (C) Aaron Holtzman - May 1999
+ *      Modifications Copyright (C) Stan Seibert - July 2000
+ *
+ *  This file is part of libao, a cross-platform audio output library.  See
+ *  README for a history of this source code.
+ *
+ *  libao is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2, or (at your option)
+ *  any later version.
+ *
+ *  libao is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with GNU Make; see the file COPYING.  If not, write to
+ *  the Free Software Foundation, 675 Mass Ave, Cambridge, MA 02139, USA.
+ *
+ */
+
+/* Set type sizes for this platform (Requires Autoconf) */
+
+#ifndef __OS_TYPES_H__
+#define __OS_TYPES_H__
+
+typedef unsigned char     uint_8;
+typedef unsigned short uint_16;
+typedef unsigned int uint_32;
+typedef signed   char     sint_8;
+typedef signed   short sint_16;
+typedef signed   int sint_32;
+
+#endif /* __OS_TYPES_H__ */
