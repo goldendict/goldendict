goldendict/goldendict

commit 46b4151cc42f0867be5dedbac99a3568485cd807

improve: replace all the deprecated method before 5.15

epwing_book.cc
@@ -1732,8 +1732,7 @@ QByteArray EpwingBook::handleReference( EB_Hook_Code code, const unsigned int * 
     if( refOpenCount > refCloseCount )
       return QByteArray();
 
-    QString str;
-    str.sprintf( "<R%i>", refOpenCount );
+    QString str=QString( "<R%1>").arg( refOpenCount );
     refOpenCount += 1;
     return str.toUtf8();
   }
@@ -1747,7 +1746,7 @@ QByteArray EpwingBook::handleReference( EB_Hook_Code code, const unsigned int * 
   refOffsets.append( argv[ 2 ] );
 
   QString str;
-  str.sprintf( "</R%i>", refCloseCount );
+  str.asprintf( "</R%i>", refCloseCount );
   refCloseCount += 1;
 
   return str.toUtf8();

aard.cc
@@ -941,8 +941,7 @@ vector< sptr< Dictionary::Class > > makeDictionaries(
           quint16 volumes = qFromBigEndian( dictHeader.totalVolumes );
           if( volumes > 1 )
           {
-            QString ss;
-            ss.sprintf( " (%i/%i)", qFromBigEndian( dictHeader.volume ), volumes );
+            QString ss=QString( " (%1/%2)").arg( qFromBigEndian( dictHeader.volume ), volumes );
             dictName += ss.toLocal8Bit().data();
           }
 

article_netmgr.cc
@@ -32,7 +32,7 @@ using std::string;
 
     connect( baseReply, SIGNAL( metaDataChanged() ), this, SLOT( applyMetaData() ) );
 
-    connect( baseReply, SIGNAL( error( QNetworkReply::NetworkError) ),
+    connect( baseReply, SIGNAL( errorOccurred( QNetworkReply::NetworkError) ),
              this, SLOT( applyError( QNetworkReply::NetworkError ) ) );
 
     connect( baseReply, SIGNAL( readyRead() ), this, SLOT( readDataFromBase() ) );
@@ -113,8 +113,8 @@ using std::string;
                   baseReply->attribute( QNetworkRequest::HttpPipeliningWasUsedAttribute ) );
     setAttribute( QNetworkRequest::BackgroundRequestAttribute,
                   baseReply->attribute( QNetworkRequest::BackgroundRequestAttribute ) );
-    setAttribute( QNetworkRequest::SpdyWasUsedAttribute,
-                  baseReply->attribute( QNetworkRequest::SpdyWasUsedAttribute ) );
+    setAttribute( QNetworkRequest::Http2WasUsedAttribute,
+                  baseReply->attribute( QNetworkRequest::Http2WasUsedAttribute ) );
 
     emit metaDataChanged();
   }
@@ -133,7 +133,7 @@ using std::string;
   void AllowFrameReply::applyError( QNetworkReply::NetworkError code )
   {
     setError( code, baseReply->errorString() );
-    emit error( code );
+    emit errorOccurred( code );
   }
 
   void AllowFrameReply::readDataFromBase()
@@ -275,7 +275,7 @@ QNetworkReply * ArticleNetworkAccessManager::createRequest( Operation op,
   if ( hideGoldenDictHeader && req.url().scheme().startsWith("http", Qt::CaseInsensitive))
   {
     QNetworkRequest newReq( req );
-    newReq.setRawHeader("User-Agent", req.rawHeader("User-Agent").replace(qApp->applicationName(), ""));
+    newReq.setRawHeader("User-Agent", req.rawHeader("User-Agent").replace(qApp->applicationName().toUtf8(), ""));
     reply = QNetworkAccessManager::createRequest( op, newReq, outgoingData );
   }
 
@@ -332,8 +332,8 @@ sptr< Dictionary::DataRequest > ArticleNetworkAccessManager::getResource(
 
     // See if we have some dictionaries muted
 
-    QSet< QString > mutedDicts =
-        QSet< QString >::fromList( Utils::Url::queryItemValue( url, "muted" ).split( ',' ) );
+    QStringList mutedDictLists=Utils::Url::queryItemValue( url, "muted" ).split( ',' );
+    QSet< QString > mutedDicts ( mutedDictLists.begin(),mutedDictLists.end());
 
     // Unpack contexts
 
@@ -521,7 +521,7 @@ void ArticleResourceReply::readyReadSlot()
 void ArticleResourceReply::finishedSlot()
 {
   if (req->dataSize() < 0) {
-    emit error(ContentNotFoundError);
+    emit errorOccurred(ContentNotFoundError);
     setError(ContentNotFoundError, "content not found");
   }
 

articleview.cc
@@ -864,15 +864,17 @@ bool ArticleView::eventFilter( QObject * obj, QEvent * ev )
         int delta = result == Gestures::SWIPE_UP ? -120 : 120;
         QWidget *widget = static_cast< QWidget * >( obj );
 
+        QPoint angleDelta(0, delta);
+        QPoint pixelDetal;
         QWidget *child = widget->childAt( widget->mapFromGlobal( pt ) );
         if( child )
         {
-          QWheelEvent whev( child->mapFromGlobal( pt ), pt, delta, Qt::NoButton, Qt::NoModifier );
+          QWheelEvent whev( child->mapFromGlobal( pt ), pt,  pixelDetal,angleDelta, Qt::NoButton, Qt::NoModifier,Qt::NoScrollPhase,false);
           qApp->sendEvent( child, &whev );
         }
         else
         {
-          QWheelEvent whev( widget->mapFromGlobal( pt ), pt, delta, Qt::NoButton, Qt::NoModifier );
+          QWheelEvent whev( widget->mapFromGlobal( pt ), pt,pixelDetal, angleDelta,Qt::NoButton, Qt::NoModifier,Qt::NoScrollPhase,false );
           qApp->sendEvent( widget, &whev );
         }
       }

btreeidx.cc
@@ -1437,16 +1437,17 @@ void BtreeIndex::getHeadwordsFromOffsets( QList<uint32_t> & offsets,
 
     for( unsigned i = 0; i < result.size(); i++ )
     {
-      QList< uint32_t >::Iterator it = qBinaryFind( begOffsets, endOffsets,
-                                                    result.at( i ).articleOffset );
+      uint32_t articleOffset =   result.at(i).articleOffset;
+      QList<uint32_t>::Iterator  it = std::lower_bound( begOffsets, endOffsets,
+                                                    articleOffset );
 
-      if( it != offsets.end() )
+      if( it!=offsets.end())
       {
         if( isCancelled && Utils::AtomicInt::loadAcquire( *isCancelled ) )
           return;
 
         headwords.append(  QString::fromUtf8( ( result[ i ].prefix + result[ i ].word ).c_str() ) );
-        offsets.erase( it );
+        offsets.erase( it);
         begOffsets = offsets.begin();
         endOffsets = offsets.end();
       }

dictionary.cc
@@ -487,13 +487,13 @@ bool needToRebuildIndex( vector< string > const & dictionaryFiles,
       ZipFile::SplitZipFile zf( name );
       if( !zf.exists() )
         return true;
-      ts = zf.lastModified().toTime_t();
+      ts = zf.lastModified().toSecsSinceEpoch();
     }
     else
     {
       if ( !fileInfo.exists() )
         return true;
-      ts = fileInfo.lastModified().toTime_t();
+      ts = fileInfo.lastModified().toSecsSinceEpoch();
     }
 
     if ( ts > lastModified )
@@ -505,7 +505,7 @@ bool needToRebuildIndex( vector< string > const & dictionaryFiles,
   if ( !fileInfo.exists() )
     return true;
 
-  return fileInfo.lastModified().toTime_t() < lastModified;
+  return fileInfo.lastModified().toSecsSinceEpoch() < lastModified;
 }
 
 QString generateRandomDictionaryId()

dictionarybar.cc
@@ -211,7 +211,7 @@ void DictionaryBar::showContextMenu( QContextMenuEvent * event, bool extended )
   {
     QString command( editDictionaryCommand );
     command.replace( "%GDDICT%", "\"" + dictFilename + "\"" );
-    if( !QProcess::startDetached( command ) )
+    if( !QProcess::startDetached( command ,QStringList()) )
       QApplication::beep();
   }
 

epwing.cc
@@ -957,9 +957,7 @@ vector< sptr< Dictionary::Class > > makeDictionaries(
   vector< sptr< Dictionary::Class > > dictionaries;
 
   vector< string > dictFiles;
-  QByteArray catName;
-  catName += QDir::separator();
-  catName += "catalogs";
+  QByteArray catName = QString("%1catalogs").arg(QDir::separator()).toUtf8();
 
   for( vector< string >::const_iterator i = fileNames.begin(); i != fileNames.end();
        ++i )

commit e06f86504830113ea695c91eef1fa0610b385cc0

fix:fix epwing dictionary link display incorrectly
this bug was introduced by the commit 46b4151cc42f08

epwing_book.cc
@@ -13,9 +13,6 @@
 #include "wstring_qt.hh"
 #include "folding.hh"
 #include "epwing_charmap.hh"
-#if (QT_VERSION >= QT_VERSION_CHECK(6,0,0))
-#include <QtCore5Compat>
-#endif
 
 #if defined( Q_OS_WIN32 ) || defined( Q_OS_MAC )
 #define _FILE_OFFSET_BITS 64
@@ -1207,8 +1204,8 @@ void EpwingBook::finalizeText( QString & text )
   // Replace references
 
   int pos = 0;
-  QRegularExpression reg1( "<R[^<]*>" );
-  QRegularExpression reg2( "</R[^<]*>" );
+  QRegularExpression reg1( "<R[^<]*>", QRegularExpression::CaseInsensitiveOption );
+  QRegularExpression reg2( "</R[^<]*>", QRegularExpression::CaseInsensitiveOption );
 
   EContainer cont( this, true );
 
@@ -1751,8 +1748,7 @@ QByteArray EpwingBook::handleReference( EB_Hook_Code code, const unsigned int * 
   refPages.append( argv[ 1 ] );
   refOffsets.append( argv[ 2 ] );
 
-  QString str;
-  str.asprintf( "</R%i>", refCloseCount );
+  QString str = QString( "</R%1>" ).arg( refCloseCount );
   refCloseCount += 1;
 
   return str.toUtf8();