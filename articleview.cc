40dd01755e27e3ee4fa642421634d5374f929f30
Igor Kushnir <igorkuo@gmail.com>
2022-06-11 20:57:52 +0300

    Always jump to current article before reloading
    MainWindow calls ArticleView::reload() when the group list is updated.
    This updating may add/remove/reorder dictionaries in the active group.
    
    MainWindow also calls ArticleView::reload() when the display or addon
    style changes.
    
    In both of the above scenarios uncontrolled jumps and current article
    change can occur (see also the parent commit message).
    
    Move setting articleToJump from above the only ArticleView's reload()
    call into ArticleView::reload() itself.

--- articleview.cc
+++ articleview.cc
@@ -1690,6 +1690,12 @@ void ArticleView::forward()
   ui.definition->forward();
 }
 
+void ArticleView::reload()
+{
+  articleToJump = getCurrentArticle();
+  ui.definition->reload();
+}
+
 bool ArticleView::hasSound()
 {
   QVariant v = ui.definition->page()->mainFrame()->evaluateJavaScript( "gdAudioLinks.first" );
@@ -2522,7 +2528,6 @@ void ArticleView::receiveExpandOptionalParts( bool expand )
 void ArticleView::switchExpandOptionalParts()
 {
   expandOptionalParts = !expandOptionalParts;
-  articleToJump = getCurrentArticle();
   emit setExpandMode( expandOptionalParts );
   reload();
 }
