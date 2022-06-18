7a43cc366e219d05c8cefc320505914801939dee
Xiao YiFang <yifang.xiao@gmail.com>
2022-06-18 09:09:40 +0800

    use qmutex instead of mutex(qmutexrecursive)

--- chunkedstorage.cc
+++ chunkedstorage.cc
@@ -137,7 +137,7 @@ char * Reader::getBlock( uint32_t address, vector< char > & chunk )
   // Read and decompress the chunk
   {
     // file.seek( offsets[ chunkIdx ] );
-    Mutex::Lock _( file.lock );
+    QMutexLocker _( &file.lock );
     auto bytes = file.map( offsets[ chunkIdx ], 8 );
     if( bytes == nullptr )
       throw mapFailed();
