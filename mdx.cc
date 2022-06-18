f65d47602abac444034c52453282184134ce8d26
Xiao YiFang <yifang.xiao@gmail.com>
2022-06-18 16:24:30 +0800

    concurrent excepton

--- mdx.cc
+++ mdx.cc
@@ -911,14 +911,20 @@ void MdxDictionary::loadArticle( uint32_t offset, string & articleText, bool noF
   QString articleId;
   articleId.setNum( ( quint64 )pRecordInfo, 16 );
 
-  ScopedMemMap compressed( dictFile, recordInfo.compressedBlockPos, recordInfo.compressedBlockSize );
-  if ( !compressed.startAddress() )
-    throw exCorruptDictionary();
-
   QByteArray decompressed;
-  if ( !MdictParser::parseCompressedBlock( recordInfo.compressedBlockSize, ( char * )compressed.startAddress(),
-                                           recordInfo.decompressedBlockSize, decompressed ) )
-    throw exCorruptDictionary();
+
+  {
+    Mutex::Lock _( idxMutex );
+    ScopedMemMap compressed( dictFile, recordInfo.compressedBlockPos, recordInfo.compressedBlockSize );
+    if( !compressed.startAddress() )
+      throw exCorruptDictionary();
+
+    if( !MdictParser::parseCompressedBlock( recordInfo.compressedBlockSize,
+                                            (char *)compressed.startAddress(),
+                                            recordInfo.decompressedBlockSize,
+                                            decompressed ) )
+      throw exCorruptDictionary();
+  }
 
   QString article = MdictParser::toUtf16( encoding.c_str(),
                                           decompressed.constData() + recordInfo.recordOffset,
