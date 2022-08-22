cee57d69ce859f4d54497be73d45682459c2bc2d
Xiao YiFang <yifang.xiao@gmail.com>
2022-08-22 20:22:14 +0800

    opt: customized about:blank page

--- mainwindow.cc
+++ mainwindow.cc
@@ -1725,6 +1725,8 @@ ArticleView * MainWindow::createNewTab( bool switchToIt,
 
   view->setZoomFactor( cfg.preferences.zoomFactor );
 
+  view->showDefinition( "about:blank", Instances::Group::HelpGroupId );
+
 #ifdef Q_OS_WIN32
   view->installEventFilter( this );
 #endif
