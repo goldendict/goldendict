19378d2f83d9433daf6eb0ab4b2f9b4946c3746a
Xiao YiFang <yifang.xiao@gmail.com>
2022-09-04 14:17:25 +0800

    opt:iframe max-height value

--- scripts/gd-custom.js
+++ scripts/gd-custom.js
@@ -49,7 +49,7 @@
         });
 
         //monitor iframe height.
-        $('iframe').iFrameResize({ checkOrigin:false,maxHeight :1500,scrolling:true,warningTimeout:0,minHeight :250});
+        $('iframe').iFrameResize({ checkOrigin:false,maxHeight :800,scrolling:true,warningTimeout:0,minHeight :250,log:true});
     });
 })($_$);
 
