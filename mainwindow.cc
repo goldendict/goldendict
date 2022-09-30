5548ef7d2575dd8be390e75d9c04271abec65fe3
Igor Kushnir <igorkuo@gmail.com>
2022-09-30 21:50:27 +0300

    Save Article: add gdpicture:// URL scheme support
    Replace each custom gdpicture URL with the corresponding image URL. This
    way clicking on a resized DSL image opens the full-size image in a
    browser, which is what occurs in GoldenDict itself.

--- mainwindow.cc
+++ mainwindow.cc
@@ -3929,6 +3929,9 @@ private:
       urlString.remove( 0, 1 );
 
       QUrl url( urlString );
+      if( url.scheme() == QLatin1String( "gdpicture" ) )
+        url.setScheme( "bres" );
+
       QString host = url.host();
       QString resourcePath = Qt4x5::Url::fullPath( url );
 
@@ -3988,7 +3991,7 @@ private:
   bool filterAndCollectResources( QString & linkSource, vector< Resource > & resourcesToDownload,
                                   QString const & pathFromLinkSourceToDestinationDir )
   {
-    static QRegExp const rx1( "'(?:bres|gico|gdau|gdvideo|qrcx?|file)://[^']+'" );
+    static QRegExp const rx1( "'(?:bres|gdpicture|gico|gdau|gdvideo|qrcx?|file)://[^']+'" );
     static QRegExp const rx2( rx1.pattern().replace( '\'', '"' ) );
 
     bool const modified1 = filterAndCollectResources( linkSource, resourcesToDownload, rx1,
