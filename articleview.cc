3d099abf4436bab2600058595f1fbc49c1992a2a
Igor Kushnir <igorkuo@gmail.com>
2023-03-29 21:02:03 +0300

    Fix -Wsign-compare GCC warning
    GCC's wchar_t is signed on GNU/Linux:
        warning: comparison of integer expressions of different signedness: ‘const gd::wchar’ {aka ‘const wchar_t’} and ‘unsigned int’ [-Wsign-compare]
    
    The comment at
    https://github.com/goldendict/goldendict/issues/978#issuecomment-383374445
    states:
        AFAIK, Unicode range is 0-0x10FFFF. Therefore *nextChar can't be
        negative for 32-bit wchar_t.
    
    static_cast-ing 0x10000U to gd::wchar is risky if it is 16-bit.
    Simply remove the unsigned (U) suffix. Utf8::encode() also compares a
    gd::wchar to 0x10000 and apparently does not cause MSVC compiler
    warnings, even though gd::wchar aliases unsigned int on Windows.

--- articleview.cc
+++ articleview.cc
@@ -159,7 +159,7 @@ public:
 
     for( size_t left = baseText.size(); left; )
     {
-      if( *nextChar >= 0x10000U )
+      if( *nextChar >= 0x10000 )
       {
         // Will be translated into surrogate pair
         normText.push_back( *nextChar );
